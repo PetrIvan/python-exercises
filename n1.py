7da89c845b8373fb1289b0f8f44b838c1d872304
Petr Ivan <59367693+PetrIvan@users.noreply.github.com>
2023-03-12 15:58:57 +0100

    Update n1
    Added checking for the second exercise.

--- n1.py
+++ n1.py
@@ -1,3 +1,13 @@
+import random
+import string
+
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+
+
 def check_p1_e1(dochazka, pocet_ucastniku):
     assert dochazka is not None, "Seznam není definován!"
     assert pocet_ucastniku is not None, "Počet účastníků není definován!"
@@ -15,5 +25,14 @@ def check_p1_e1(dochazka, pocet_ucastniku):
     print("\x1b[32mVšechny testy proběhly úspěšně!\x1b[0m")
 
 
-def check_p1_e2():
+def check_p1_e2(func):
+    zavodnice = []
+    for i in range(random.randrange(8, 15)):
+        zavodnice.append(get_random_string(8))
+    jmeno_vitezky, jmeno_druhe, jmeno_treti, pocet_zavodnic = func(zavodnice)
+    assert jmeno_vitezky == zavodnice[0], "Jméno vítězky neodpovídá skutečné vítězce!"
+    assert jmeno_druhe == zavodnice[1], "Jméno druhé neodpovídá skutečné druhé závodnici!"
+    assert jmeno_treti == zavodnice[2], "Jméno třetí neodpovídá skutečné třetí závodnici!"
+    assert pocet_zavodnic == len(
+        zavodnice), "Počet závodnic neodpovídá skutečnému počtu závodnic!"
     print("\x1b[32mVšechny testy proběhly úspěšně!\x1b[0m")
